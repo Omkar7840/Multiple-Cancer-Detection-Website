7181139a0b981be7e8e7f803001187a163b5ae4c
Omkar Babar <118367159+Omkar7840@users.noreply.github.com>
2025-09-30 19:22:48 +0530

    to render main.py

--- backend/main.py
+++ backend/main.py
@@ -107,4 +107,7 @@ def analyze(cancer_type):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
